c1c292c77ce2e8be31258bc5e415ba51172e2eed
Niperx <79987802+Niperx@users.noreply.github.com>
2021-08-09 23:51:12 +1100

    upd

--- bot_start.py
+++ bot_start.py
@@ -15,10 +15,13 @@ import sqlite3
 import threading
 import random
 from PIL import Image, ImageDraw, ImageFont
+import io
+from urllib.request import urlopen
 #pip install Pillow
 
 
 from settings import *
+from test import *
 
 vk_session = vk_api.VkApi(token=token)
 
@@ -26,7 +29,7 @@ session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 upload = VkUpload(vk_session)
 
-def send_message_to_user(user, msg, attachment=''):
+def send_user(user, msg, attachment=''):
 	vk_session.method('messages.send',
 		{
 		'user_id': user,
@@ -47,6 +50,16 @@ def send_user_reply(user, msg, reply='', attachment=''):
 		}
 	)
 
+def send_chat(chat, msg, attachment=''):
+	vk_session.method('messages.send',
+		{
+		'chat_id': chat,
+		'message': msg,
+		'random_id': 0,
+		'attachment': attachment
+		}
+	)
+
 def send_chat_reply(chat, msg, reply='', attachment=''):
 	vk_session.method('messages.send',
 		{
@@ -59,7 +72,6 @@ def send_chat_reply(chat, msg, reply='', attachment=''):
 	)
 
 def photo_messages(img):
-	img = 'images/' + img + '.jpg'
 	url = session_api.photos.getMessagesUploadServer(peer_id=0)['upload_url']
 	res = requests.post(url, files={'photo': open(img, 'rb')}).json()
 	result = session_api.photos.saveMessagesPhoto(**res)[0]
@@ -72,22 +84,85 @@ def get_user_name(user_id):
 	fullname = [user[0]['first_name'], user[0]['last_name']]
 	return fullname
 
-def get_random_match():
-	url = 'https://ru.dotabuff.com/matches'
 
-	response = requests.get(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'})
-	soup = BeautifulSoup(response.text, 'html.parser')
 
-	matches = []
-	for el in soup.select('tr'):
-		title = el.select('td > a')
-		for x in title:
-			for y in x:
-				y = re.findall('\d+', y)
-				if len(y) != 0:
-					matches.append(y[0])
 
-	return random.choice(matches)
+def create_test():
+	with open("match.json", 'r', encoding='utf-8') as read_message:
+		message = json.load(read_message)
+
+	my_list = []
+	for item in message.keys():
+		my_list.append(item)
+
+	hero_list = []
+	chk = 3
+	while chk != 0: 
+		hero = random.choice(my_list)
+		hero_list.append(hero)
+		my_list.remove(hero)
+		chk -= 1
+
+	current_hero = random.choice(hero_list)
+
+	for url in message[current_hero]:
+		hero_img = url
+		print('Картинка: '+hero_img)
+		break
+
+	img_bag = Image.open('bag.jpg')
+
+	url_hero = message[current_hero][0]
+	headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
+	img_hero = Image.open(requests.get(url_hero, stream=True, headers = headers).raw)
+
+	wid, yid = 6, 5
+	for url in message[current_hero]:
+		if current_hero not in url.replace('-', ' '):
+			print(url)
+			maxsize = (60, 43)
+			im = Image.open(requests.get(url, stream=True, headers = headers).raw)
+			im.thumbnail(maxsize, Image.ANTIALIAS)
+			img_bag.paste(im, (wid, yid))
+
+			wid += 66
+			if wid >= 150:
+				yid += 48
+				wid = 6
+
+	img_bag.save('bag1.jpg') # Отправить в чат
+
+	img_bg = Image.open('bg.jpg')
+	chk = 3
+	wid, yid = 2, 2
+	while chk != 0: 
+		hero = random.choice(hero_list)
+
+		maxsize = (60, 43)
+		im = Image.open(requests.get(message[hero][0], stream=True, headers = headers).raw)
+		img_bg.paste(im, (wid, yid))
+		chk -= 1
+		wid += 130
+
+		hero_list.remove(hero)
+
+	img_bg.save('bg1.jpg') # Отправить в чат
+
+	result = [current_hero]
+
+	with open("result.json", 'w', encoding='utf-8') as write_message:
+		json.dump(result, write_message, ensure_ascii=False, indent=4)
+
+
+	x1 = photo_messages('bg1.jpg')
+	x2 = photo_messages('bag1.jpg')
+	# send_user(user_id, 'Угадайте героя по билду', photo_messages('bg1.jpg'))
+	# send_user(user_id, '', photo_messages('bag1.jpg'))
+
+
+	send_chat(10, 'Угадайте героя по билду', x1)
+	send_chat(10, '', x2)
+
 
 for event in longpoll.listen():
 	if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
@@ -102,4 +177,67 @@ for event in longpoll.listen():
 			text = ''
 			fullname = get_user_name(event.user_id)
 			print('Кто: '+fullname[0]+' '+fullname[1])
-			print(event.message_id)
+			print('Номер сообщения: '+str(event.message_id))
+
+			if response == 'мем' and event.user_id == 201044121:
+
+				create_test()
+
+
+			with open("result.json", 'r', encoding='utf-8') as read_message:
+				message = json.load(read_message)
+
+			if response == message[0]:
+				# Человек получает поинты
+				with open("stats.json", 'r', encoding='utf-8') as read_message:
+					msg = json.load(read_message)
+
+				check = 0
+				for player in msg.keys():
+					if player == event.user_id:
+						check = 2
+					else:
+						check = 1
+
+				if check == 1:
+					msg.update({event.user_id : 1})
+				elif check == 2:
+					popit = msg.pop(event.user_id)
+					print(popit)
+					msg.update({event.user_id : popit+1})
+
+				with open("stats.json", 'w', encoding='utf-8') as write_message:
+					json.dump(msg, write_message, ensure_ascii=False, indent=4)
+
+				text = 'Это правильный ответ!\nОжидайте следующий билд...'
+				send_chat_reply(10, text, event.message_id)
+
+				get_match_info()
+				time.sleep(35)
+				create_test()
+				
+
+		if event.from_user and not (event.from_me):
+			response = event.text.lower()
+			text = ''
+			fullname = get_user_name(event.user_id)
+			print('Кто: '+fullname[0]+' '+fullname[1])
+			print('Номер сообщения: '+str(event.message_id))
+
+			if response == 'мем' and event.user_id == 201044121:
+
+				create_test(event.user_id)
+
+
+			with open("result.json", 'r', encoding='utf-8') as read_message:
+				message = json.load(read_message)
+
+			if response == message[0]:
+				# Человек получает поинты
+				text = 'Это правильный ответ!\nОжидайте следующий билд...'
+				send_user_reply(event.user_id, text, event.message_id)
+
+				get_match_info()
+				create_test(event.user_id)
+
+				time.sleep(35)
--- test.py
+++ test.py
@@ -7,82 +7,105 @@ import json
 main = 'https://ru.dotabuff.com'
 url = 'https://ru.dotabuff.com/matches/'
 
-# ВЗЯТЬ СЛУЧАЙНЫЙ МАТЧ
+def get_match_info():
 
-response = requests.get(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'})
-soup = BeautifulSoup(response.text, 'html.parser')
-# quotes = soup.find_all('tr')
+	# ВЗЯТЬ СЛУЧАЙНЫЙ МАТЧ
 
-# for n in en
-# # print(quotes)
+	response = requests.get(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'})
+	soup = BeautifulSoup(response.text, 'html.parser')
+	# quotes = soup.find_all('tr')
 
-# print(soup)
-matches = []
-for el in soup.select('tr'):
-	title = el.select('td > a')
-	for x in title:
-		for y in x:
-			y = re.findall('\d+', y)
-			if len(y) != 0:
-				matches.append(y[0])
+	# for n in en
+	# # print(quotes)
 
-match = random.choice(matches)
+	# print(soup)
+	matches = []
+	for el in soup.select('tr'):
+		title = el.select('td > a')
+		for x in title:
+			for y in x:
+				y = re.findall('\d+', y)
+				if len(y) != 0:
+					matches.append(y[0])
 
-
-print(match)
-
-# ВЗЯТЬ ИМЯ И КАРТИНКУ ГЕРОЯ
-
-response1 = requests.get(url + str(match), headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'})
-soup1 = BeautifulSoup(response1.text, 'lxml')
-
-hero_info_list = {}
-for hero in soup1.find_all('tr', class_='col-hints'):
-	items_list = {}
-	current_hero = hero.find('div', class_='image-container image-container-hero image-container-icon image-container-overlay')
-	if current_hero != None:
-
-		hero_info = current_hero.find('a', href=True)['href']
-		hero_name = hero_info[hero_info.rfind('/')+1:].title()
-		hero_name = hero_name.replace('-', ' ')
-		print('Герой: ' + hero_name) # Название героя
-
-		hero_img = main + current_hero.find('img', src=True)['src']
-		print('Иконка героя: ' + hero_img + '\n') # Иконка героя
-
-		hero_items = hero.find('div', class_='player-inventory-items')
-
-		items_list.update({'hero_icon' : hero_img})
-
-		i = 1
-		for item in hero_items.find_all('div', class_='match-item-with-time'):
-			try:
-				item_info = item.find('a', href=True)['href']
-				item_name = item_info[item_info.rfind('/')+1:].title()
-				item_name = item_name.replace('-', ' ')
-				print('Предмет #'+str(i)+': ' + item_name) # Название предмета
-
-				item_img = main + item.find('img', src=True)['src']
-
-				print('Иконка предмета: ' + item_img) # Иконка предмета
+	match = random.choice(matches)
 
 
-				items_list.update({item_name : item_img})
-				
-				i += 1
-			except:
-				continue
+	print(match)
+
+	# ВЗЯТЬ ИМЯ И КАРТИНКУ ГЕРОЯ
+
+	response1 = requests.get(url + str(match), headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'})
+	soup1 = BeautifulSoup(response1.text, 'lxml')
+
+	hero_info_list = {}
+	for hero in soup1.find_all('tr', class_='col-hints'):
+
+		items_list = []
+		current_hero = hero.find('div', class_='image-container image-container-hero image-container-icon image-container-overlay')
+		if current_hero != None:
+
+			hero_info = current_hero.find('a', href=True)['href']
+			hero_name = hero_info[hero_info.rfind('/')+1:]
+			hero_name = hero_name.replace('-', ' ')
+			print('Герой: ' + hero_name) # Название героя
+
+			hero_img = main + current_hero.find('img', src=True)['src']
+			print('Иконка героя: ' + hero_img + '\n') # Иконка героя
+
+			hero_items = hero.find('div', class_='player-inventory-items')
+
+			items_list.append(hero_img)
+
+			i = 1
+			for item in hero_items.find_all('div', class_='match-item-with-time'):
+				try:
+					item_info = item.find('a', href=True)['href']
+					item_name = item_info[item_info.rfind('/')+1:]
+					item_name = item_name.replace('-', ' ')
+					print('Предмет #'+str(i)+': ' + item_name) # Название предмета
+
+					item_img = main + item.find('img', src=True)['src']
+
+					print('Иконка предмета: ' + item_img) # Иконка предмета
 
 
-		print(items_list)
-		hero_info_list.update({hero_name : items_list})
+					items_list.append(item_img)
+					
+					i += 1
+				except:
+					continue
 
-		print('\n\n\n\n\n')
+			hero_info_list.update({hero_name : items_list})
+
+			print('\n\n\n\n\n')
 
 
 
-with open("test.json", 'w', encoding='utf-8') as write_message:
-	json.dump(hero_info_list, write_message, ensure_ascii=False, indent=4)
+	with open("match.json", 'w', encoding='utf-8') as write_message:
+		json.dump(hero_info_list, write_message, ensure_ascii=False, indent=4)
 
 # with open(core_way + "message" + str(call.from_user.id) +".json", 'r', encoding='utf-8') as read_message:
 # 	message = json.load(read_message)
+user_id = 201044121
+
+with open("stats.json", 'r', encoding='utf-8') as read_message:
+	msg = json.load(read_message)
+
+check = 0
+for player in msg.keys():
+	if player == user_id:
+		check = 2
+	else:
+		check = 1
+
+if check == 1:
+	msg.update({user_id : 1})
+elif check == 2:
+	msg[user_id] += 1
+	# popit = msg.pop(user_id)
+	# print(popit)
+	# msg.update({user_id : popit+1})
+
+with open("stats.json", 'w', encoding='utf-8') as write_message:
+	json.dump(msg, write_message, ensure_ascii=False, indent=4)
